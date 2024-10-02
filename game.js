5a3bb159ca4d9ed9b00ad7fbc674552252ae27ed
VarshaKavalipati <83327052+VarshaKavalipati@users.noreply.github.com>
2024-10-02 19:48:40 +0530

    Update game.js

--- game.js
+++ game.js
@@ -69,24 +69,24 @@ let pl2=new Player(Number(sc2.innerText),Number(sc1.innerText));
 let game=new Game();
 let gameState = true;
 document.addEventListener('keydown',function(e){
-    if(e.key=='q' && Number(sc2.innerText)>0 && gameState==true){
+    if((e.key=='q' || e.key =='Q') && Number(sc2.innerText)>0 && gameState==true){
         let s=pl1.strike();
         updateGame(sc1,sc2,0,s,0,0);
         p1im.src='assests/strik1.gif';
         ad1.play();
     }
-    else if(e.key=='p' && Number(sc1.innerText)>0 && gameState==true){
+    else if((e.key=='p' || e.key =='P') && Number(sc1.innerText)>0 && gameState==true){
         let s=pl2.strike();
         updateGame(sc1,sc2,s,0,0,0);
         p2im.src='assests/strik2.gif';
         ad2.play();
     }
-    else if(e.key=='a' && Number(sc1.innerText)>0 && gameState==true){
+    else if((e.key=='a' || e.key =='A') && Number(sc1.innerText)>0 && gameState==true){
         let h=pl1.heal();
         updateGame(sc1,sc2,0,0,h,0);
         ad3.play();
     }
-    else if(e.key=='l' && Number(sc2.innerText)>0 && gameState==true){
+    else if((e.key=='l'|| e.key =='L') && Number(sc2.innerText)>0 && gameState==true){
         let h=pl2.heal();
         updateGame(sc1,sc2,0,0,0,h);
         ad4.play();
